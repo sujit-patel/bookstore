d0552d0e3e591b5de845e5ec4ef7d7395d40ed3e
sujit patel <sujitworkclg@gmail.com>
2024-12-24 23:16:38 +0530

    update login & signup system

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import bookRoute from "./route/book.route.js";
 import cors from "cors";
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const URI = process.env.md_URI;
 
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((error) => console.log("Error connecting to MongoDB:", error));
+try {
+    mongoose.connect(URI)
+    // mongoose.connect(URI, {useNewUrlParser: true,useUnifiedTopology: true,})
+    console.log("mongoDB Connected Successful");
+} catch (error) {
+    console.log(error)
+}
 
 app.use("/book", bookRoute)
+app.use("/user", userRoute)
 
 app.listen(PORT, () => {
     console.log(`Your server is running at http://localhost:${PORT}`);
