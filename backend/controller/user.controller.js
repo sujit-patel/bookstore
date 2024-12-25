3b4285afc9feb766011a06be26d43fff059eea4b
sujit patel <sujitworkclg@gmail.com>
2024-12-25 18:49:48 +0530

    Backend Done...

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -6,7 +6,7 @@ export const signup = async (req, res) => {
         const { username, email, password } = req.body;
         const user = await User.findOne({ email });
         if (user) {
-            return res.status(400).json({ message: "User already exists" });
+            return res.status(400).json({ message: "User Already Exists. Please try Logging in." });
         }
         const hashPassword = await bcryptjs.hash(password, 10);
         const createUser = new User({
